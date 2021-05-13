524f8523a5755ab5dfe8952e6c8ea6a3a4244c9b
Kiana Long <kianajade42.kl@gmail.com>
2021-05-13 14:10:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ClickityClick from 'ClickityClick.js'
+import ClickityClick from 'ClickityClick'
 ReactDOM.render(
   <div>
     Mount Components Here
