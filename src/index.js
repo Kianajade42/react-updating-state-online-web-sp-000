d257b95ba0b40040b18f71cfd20b2552e21f7c0e
Kiana Long <kianajade42.kl@gmail.com>
2021-05-13 14:12:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
-ReactDOM.render(<ClickityClick/>
+ReactDOM.render(<ClickityClick/>,
   document.getElementById('root'));
